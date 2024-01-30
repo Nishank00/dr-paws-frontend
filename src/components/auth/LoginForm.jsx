c8d81990fc4325c96cf9c3a01c2ebcf1145d2794
Mohammad Salman Khan <salman.khan@gokozo.com>
2024-01-30 12:19:25 +0530

    Minor change

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -22,72 +22,70 @@ const LoginForm = () => {
     console.table(form);
   };
 
-  const RenderPage1 = () => (
-    <div className="">
-      <div className="">
-        <h5>Log in to book a visit</h5>
-        <p>Add your phone number. We'll send you a verification code.</p>
-      </div>
-
-      <div className="">
-        <TextInput
-          placeholder={"Email"}
-          name="email"
-          value={form.email}
-          onChange={formValueChanged}
-        />
-        <TextInput
-          placeholder={"Mobile Number"}
-          name="mobile_number"
-          value={form.mobile_number}
-          onChange={formValueChanged}
-        />
-
-        <Button label="Next" onClick={handleNext} />
-      </div>
-      <div className="">
-        <p>Don’t have an Account? Sign Up</p>
-      </div>
-    </div>
-  );
-
-  const RenderPage2 = () => (
-    <div className="">
-      <div className="">
-        <h5>Generate your password</h5>
-        <p>Both fields should match, to successfully create the password</p>
-      </div>
-
-      <div className="">
-        <TextInput
-          type={"password"}
-          placeholder={"Password"}
-          name="password"
-          value={form.password}
-          onChange={formValueChanged}
-        />
-        <TextInput
-          type={"password"}
-          placeholder={"Confirm Password"}
-          name="confirm_password"
-          value={form.confirm_password}
-          onChange={formValueChanged}
-        />
-
-        <Button label="Submit" />
-      </div>
-      <div className="">
-        <p>Don’t have an Account? Sign Up</p>
-      </div>
-    </div>
-  );
-
   const renderPage = (page) => {
     switch (page) {
       case 1:
-        return <RenderPage1 />;
+        return (
+          <div className="">
+            <div className="">
+              <h5>Log in to book a visit</h5>
+              <p>Add your phone number. We'll send you a verification code.</p>
+            </div>
+
+            <div className="">
+              <TextInput
+                placeholder={"Email"}
+                name="email"
+                value={form.email}
+                onChange={formValueChanged}
+              />
+              <TextInput
+                placeholder={"Mobile Number"}
+                name="mobile_number"
+                value={form.mobile_number}
+                onChange={formValueChanged}
+              />
+
+              <Button label="Next" onClick={handleNext} />
+            </div>
+            <div className="">
+              <p>Don’t have an Account? Sign Up</p>
+            </div>
+          </div>
+        );
       case 2:
-        return <RenderPage2 />;
+        return (
+          <div className="">
+            <div className="">
+              <h5>Generate your password</h5>
+              <p>
+                Both fields should match, to successfully create the password
+              </p>
+            </div>
+
+            <div className="">
+              <TextInput
+                type={"password"}
+                placeholder={"Password"}
+                name="password"
+                value={form.password}
+                onChange={formValueChanged}
+              />
+              <TextInput
+                type={"password"}
+                placeholder={"Confirm Password"}
+                name="confirm_password"
+                value={form.confirm_password}
+                onChange={formValueChanged}
+              />
+
+              <Button label="Submit" />
+            </div>
+            <div className="">
+              <p>Don’t have an Account? Sign Up</p>
+            </div>
+          </div>
+        );
       default:
         return null;
     }
