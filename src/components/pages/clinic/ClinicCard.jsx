1858fade172eaf89d1865fce92349f073b26e030
akash8822631 <akash@gokozo.com>
2024-02-01 19:01:44 +0530

    clinic module updated

--- src/components/pages/clinic/ClinicCard.jsx
+++ src/components/pages/clinic/ClinicCard.jsx
@@ -1,9 +1,11 @@
+"use client"
 import { useRouter } from "next/navigation";
 import React from "react";
 
-const ClinicCard = ({ imageUrl ,name,id}) => {
+const ClinicCard = ( { imageUrl, name, id } ) =>
+{
 
-  const router=useRouter();
+  const router = useRouter();
   return (
     // <div className="bg-primary3 rounded-lg">
     //   <div
@@ -26,24 +28,25 @@ const ClinicCard = ({ imageUrl ,name,id}) => {
     //     Book a Visit
     //   </div>
     // </div>
-    <div onClick={()=>router.push(`/clinics/overview/${id}`)} className="justify-between items-stretch shadow-sm bg-orange-50 flex max-w-[330px] flex-col rounded">
-    <img
-      loading="lazy"
-      srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&"
-      className="aspect-[1.38] object-contain object-center w-full overflow-hidden"
-    />
-    <span className="items-stretch bg-orange-50 flex flex-col px-5 py-6">
-      <div className="text-slate-700 text-xl font-semibold leading-5 tracking-normal">
-       {name}
-      </div>
-      <div className="text-slate-700 text-sm leading-4 tracking-normal whitespace-nowrap mt-3">
-        OPD | Surgery
-      </div>
-    </span>
-    <button className="text-white text-base font-bold leading-5 tracking-normal whitespace-nowrap justify-center items-center bg-slate-500 w-full px-16 py-4">
-      Book a Visit
-    </button>
-  </div>
+    <div onClick={() => router.push( `/clinics/overview/${ id }` )} className="justify-between items-stretch shadow-sm bg-orange-50 flex max-w-[330px] flex-col rounded overflow-hidden group hover:cursor-pointer">
+      <img
+        onClick={() => router.push( `/clinics/overview/${ id }` )}
+        loading="lazy"
+        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/3eee728a04ec723267d97cb874360dd881bd0e4eaf04fbd0d6e5b24362709013?apiKey=22a36eade5734692978208fb0d2f5c62&"
+        className="aspect-[1.38] object-contain object-center w-full overflow-hidden transform transition-transform group-hover:scale-110"
+      />
+      <span className="items-stretch bg-orange-50 flex flex-col px-5 py-6">
+        <div className="text-slate-700 text-xl font-semibold leading-5 tracking-normal">
+          {name}
+        </div>
+        <div className="text-slate-700 text-sm leading-4 tracking-normal whitespace-nowrap mt-3">
+          OPD | Surgery
+        </div>
+      </span>
+      <button className="text-white text-base font-bold leading-5 tracking-normal whitespace-nowrap justify-center items-center bg-slate-500 w-full px-16 py-4">
+        Book a Visit
+      </button>
+    </div>
   );
 };
 
--- src/components/pages/clinic/OverviewTabs.jsx
+++ src/components/pages/clinic/OverviewTabs.jsx
@@ -6,17 +6,46 @@ import TabThree from './TabThree';
 import GallaryBox from './GallaryBox';
 import Slider from './Slider';
 import { useParams } from 'next/navigation';
+import ClinicService from '@/services/Clinic.service';
 
-const OverviewTabs = () => {
-    const [activeTab, setActiveTab] = useState(1);
-    const {id}=useParams();
+const OverviewTabs = () =>
+{
+    const [ activeTab, setActiveTab ] = useState( 1 );
+    const [ clinic, setClinic ] = useState( {} );
+    const { id } = useParams();
 
-    const handleTabClick = (tabNumber) => {
-        setActiveTab(tabNumber);
+    const handleTabClick = ( tabNumber ) =>
+    {
+        setActiveTab( tabNumber );
     }
-    useEffect(()=>{
-        console.log("id=>",id)
-    },[])
+
+    const getDataById = () =>
+    {
+        ClinicService.getData( { id } ).then( ( r ) =>
+        {
+            if ( r.data.status )
+            {
+                setClinic( r.data.data[ 0 ] );
+                console.log( "data=>", r.data.data[ 0 ] )
+            }
+            else
+            {
+                alert( r.data.message )
+            }
+        } )
+            .catch( () =>
+            {
+                console.log( err )
+            } )
+    }
+    useEffect( () =>
+    {
+        if ( id )
+        {
+            getDataById();
+        }
+
+    }, [ id ] )
 
     return (
         <div className='w-[100%] m-auto flex-auto justify-center  items-center'>
@@ -25,14 +54,14 @@ const OverviewTabs = () => {
                     <GallaryBox />
                 }
             </div>
-          
+
             <span className="self-center flex w-full max-w-[1042px]  items-stretch justify-between gap-5  max-md:max-w-full max-md:flex-wrap max-md:mt-10">
                 <div className="px-5 max-md:max-w-full">
                     <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
                         <div className="flex flex-col items-stretch w-[81%] max-md:w-full max-md:ml-0">
                             <span className="flex grow flex-col items-stretch max-md:mt-7">
                                 <div className="text-slate-700 text-3xl leading-9 capitalize whitespace-nowrap">
-                                    Andheri West Clinic
+                                    {clinic.name}
                                 </div>
                                 <div className="text-slate-700 text-xl leading-6 tracking-tight whitespace-nowrap mt-7">
                                     Grooming | OPD | Surgery
@@ -51,29 +80,29 @@ const OverviewTabs = () => {
             </span>
             <div className="flex mb-4 border-b-2 mt-16">
                 <button
-                    className={`text-slate-700 text-md leading-4 tracking-normal ${activeTab === 1
+                    className={`text-slate-700 text-md leading-4 tracking-normal ${ activeTab === 1
                         ? 'border-b-2 border-blue-500 '
                         : 'text-gray-700'
                         } px-1  py-2 focus:outline-none`}
-                    onClick={() => handleTabClick(1)}
+                    onClick={() => handleTabClick( 1 )}
                 >
                     Overview
                 </button>
                 <button
-                    className={`text-slate-700 text-md leading-4 tracking-normal ${activeTab === 2
+                    className={`text-slate-700 text-md leading-4 tracking-normal ${ activeTab === 2
                         ? 'border-b-2 border-blue-500 '
                         : 'text-gray-700'
                         } px-4 py-2 ml-10 focus:outline-none`}
-                    onClick={() => handleTabClick(2)}
+                    onClick={() => handleTabClick( 2 )}
                 >
                     Photo
                 </button>
                 <button
-                    className={`text-slate-700 text-md leading-4 tracking-normal ${activeTab === 3
+                    className={`text-slate-700 text-md leading-4 tracking-normal ${ activeTab === 3
                         ? 'border-b-2 border-blue-500 '
                         : 'text-gray-700'
                         } px-4 py-2 ml-10 focus:outline-none`}
-                    onClick={() => handleTabClick(3)}
+                    onClick={() => handleTabClick( 3 )}
                 >
                     Surgery
                 </button>
@@ -81,19 +110,19 @@ const OverviewTabs = () => {
             <div className='w-[100%] m-auto flex-auto justify-center  items-center'>
                 {/* Content for Tab 1 */}
                 {activeTab === 1 && <div className="p-4">
-                    <TabOne />
+                    <TabOne {...clinic} />
                 </div>
                 }
 
                 {/* Content for Tab 2 */}
                 {activeTab === 2 && <div className="p-4" >
-                    <TabTwo />
+                    <TabTwo {...clinic} />
                 </div>
                 }
 
                 {/* Content for Tab 3 */}
                 {activeTab === 3 && <div className="p-4">
-                    <TabThree />
+                    <TabThree {...clinic} />
                 </div>
                 }
 
