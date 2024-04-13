af47e9d988a16581550d3f0f1c9e69229adfe06b
Mohammad Salman Khan <salman.khan@gokozo.com>
2024-04-13 14:09:02 +0530

    Login & register with otp

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -6,16 +6,44 @@ import { TokenService } from "@/services/Storage.service";
 import { useToast } from "../ui/ToastProvider";
 import UserService from "@/services/User.Service";
 import { UserService as UserStorageService } from "@/services/Storage.service";
+import OTPInput from "../ui/OTPInput";
+import { useLoader } from "@/components/ui/LoaderContext";
 
 const LoginForm = ({ onSuccess, signUpClicked }) => {
   // Variables
   const showToast = useToast();
+  const { startLoading, stopLoading } = useLoader();
+  const [currentPage, setCurrentPage] = useState(1);
   const [form, setForm] = useState({
+    full_name: "",
+    phone: "",
     email: "",
     password: "",
+    otp: "",
   });
 
   // Methods
+
+  const getOtp = () => {
+    startLoading();
+    const payload = { phone: form.phone };
+    AuthService.sendLoginOTP(payload)
+      .then((response) => {
+        stopLoading();
+        if (!response.data.status)
+          return showToast(response.data.message, "warning");
+      })
+      .catch((error) => {
+        stopLoading();
+        console.log(error);
+      });
+  };
+
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
+    getOtp();
+  };
+
   const formValueChanged = (e) => {
     const { name, value } = e.target;
     setForm({ ...form, [name]: value });
@@ -34,11 +62,18 @@ const LoginForm = ({ onSuccess, signUpClicked }) => {
       });
   };
 
+  const otpEntered = (otp) => {
+    console.log("otp => ", otp);
+    setForm({ ...form, otp });
+  };
+
   const loginUser = () => {
-    const payload = { email_phone: form.email, password: form.password };
+    startLoading();
+    const payload = { phone: form.phone, otp: form.otp };
 
     AuthService.login(payload)
       .then((response) => {
+        stopLoading();
         if (!response.data.status)
           return showToast(response.data.message, "warning");
 
@@ -48,47 +83,105 @@ const LoginForm = ({ onSuccess, signUpClicked }) => {
         return showToast(response.data.message, "success");
       })
       .catch((error) => {
+        stopLoading();
         console.error(error);
       });
   };
 
-  return (
-    <div className="p-10 text-primary bg-white w-full md:w-[430px]">
-      <div className="text-center">
-        <div className="mb-10">
-          <h5 className="font-bold text-3xl mb-2">Log in to book a visit</h5>
-          <p>Add your phone number. We&apos;ll send you a verification code.</p>
-        </div>
+  const renderPage = (page) => {
+    switch (page) {
+      case 1:
+        return (
+          <>
+            <div className="p-8 text-primary bg-white w-full md:w-[430px] rounded-3xl">
+              <div className="text-center">
+                <div className="mb-10">
+                  <h5 className="font-bold text-3xl mb-2">
+                    Log in to book a visit
+                  </h5>
+                  <p>
+                    Add your phone number. We&apos;ll send you a verification
+                    code.
+                  </p>
+                </div>
 
-        <div className="mb-10">
-          <TextInput
-            placeholder={"Email"}
-            type="email"
-            name="email"
-            value={form.email}
-            onChange={formValueChanged}
-          />
-          <TextInput
-            placeholder={"Password"}
-            type="password"
-            name="password"
-            value={form.password}
-            onChange={formValueChanged}
-          />
+                <div className="mb-10">
+                  <TextInput
+                    placeholder="Mobile Number"
+                    name="phone"
+                    value={form.phone}
+                    onChange={formValueChanged}
+                  />
+                  {/* <TextInput
+                    placeholder={"Email"}
+                    type="email"
+                    name="email"
+                    value={form.email}
+                    onChange={formValueChanged}
+                  />
+                  <TextInput
+                    placeholder={"Password"}
+                    type="password"
+                    name="password"
+                    value={form.password}
+                    onChange={formValueChanged}
+                  /> */}
 
-          <Button color="secondary" label="Login" onClick={loginUser} />
-        </div>
-        <div className="">
-          <p>
-            Don&apos;t have an Account?{" "}
-            <span className="cursor-pointer italic" onClick={signUpClicked}>
-              Sign Up
-            </span>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+                  <Button
+                    disabled={!form.phone}
+                    color="secondary"
+                    label="Login"
+                    onClick={handleNext}
+                  />
+                </div>
+                <div className="">
+                  <p>
+                    Don&apos;t have an Account?{" "}
+                    <span
+                      className="cursor-pointer italic"
+                      onClick={signUpClicked}
+                    >
+                      Sign Up
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+
+      case 2:
+        return (
+          <>
+            <div className="pt-16 px-10 pb-12 max-w-[430px] max-h-[460px] text-center text-primary bg-white rounded-2xl">
+              <h2 className="text-2xl font-semibold mb-2">
+                Verify your Phone Number
+              </h2>
+              <p className="text-sm mb-10">
+                To book the visit please enter the OTP sent to +91 {form.phone}.
+              </p>
+
+              <OTPInput onOTPEntered={otpEntered} />
+
+              <Button
+                disabled={form.otp?.length < 4}
+                color="secondary"
+                label="Verify and Proceed"
+                className="mt-10"
+                onClick={loginUser}
+              />
+
+              <p className="mt-14">
+                Didn&apos;t receive your code?{" "}
+                <span className="font-semibold">Resend Code</span>
+              </p>
+            </div>
+          </>
+        );
+    }
+  };
+
+  return <div>{renderPage(currentPage)}</div>;
 };
 
 export default LoginForm;
--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -4,22 +4,43 @@ import Button from "../ui/Button";
 import AuthService from "@/services/Auth.service";
 import UploadProfile from "./UploadProfile";
 import { useToast } from "../ui/ToastProvider";
+import OTPInput from "../ui/OTPInput";
+import { useLoader } from "../ui/LoaderContext";
 
 const RegisterForm = ({ onSuccess, loginClicked }) => {
   // Variables
   const showToast = useToast();
+  const { startLoading, stopLoading } = useLoader();
   const [currentPage, setCurrentPage] = useState(1);
   const [form, setForm] = useState({
     profile_image: null,
+    full_name: null,
     phone: null,
     email: null,
     password: null,
     confirm_password: null,
+    otp: null,
   });
 
   // Methods
+  const getOtp = () => {
+    startLoading();
+    const payload = { phone: form.phone, full_name: form.full_name };
+    AuthService.sendRegisterOTP(payload)
+      .then((response) => {
+        stopLoading();
+        if (!response.data.status)
+          return showToast(response.data.message, "warning");
+      })
+      .catch((error) => {
+        stopLoading();
+        console.log(error);
+      });
+  };
+
   const handleNext = () => {
     setCurrentPage(currentPage + 1);
+    getOtp();
   };
 
   const formValueChanged = (e) => {
@@ -31,17 +52,26 @@ const RegisterForm = ({ onSuccess, loginClicked }) => {
     setForm({ ...form, profile_image: filename });
   };
 
+  const otpEntered = (otp) => {
+    console.log("otp => ", otp);
+    setForm({ ...form, otp });
+  };
+
   const registerUser = () => {
+    startLoading();
     const payload = {
       profile_image: form.profile_image,
+      full_name: form.full_name,
       email: form.email,
       phone: form.phone,
       password: form.password,
       user_type: "CUSTOMER",
+      otp: form.otp,
     };
 
     AuthService.register(payload)
       .then((response) => {
+        stopLoading();
         if (!response.data.status)
           return showToast(response.data.message, "warning");
 
@@ -49,6 +79,7 @@ const RegisterForm = ({ onSuccess, loginClicked }) => {
         return showToast(response.data.message, "success");
       })
       .catch((error) => {
+        stopLoading();
         console.error(error);
       });
   };
@@ -70,19 +101,24 @@ const RegisterForm = ({ onSuccess, loginClicked }) => {
             <div className="mb-10">
               <UploadProfile onUpload={profileUploaded} />
               <TextInput
-                placeholder={"Email"}
-                name="email"
-                value={form.email}
+                placeholder={"Name"}
+                name="full_name"
+                value={form.full_name}
                 onChange={formValueChanged}
               />
               <TextInput
-                placeholder={"Mobile Number"}
+                placeholder={"Contact Number"}
                 name="phone"
                 value={form.phone}
                 onChange={formValueChanged}
               />
 
-              <Button label="Next" onClick={handleNext} />
+              <Button
+                color="secondary"
+                disabled={!form.phone || !form.full_name}
+                label="Sign Up"
+                onClick={handleNext}
+              />
             </div>
             <div className="">
               <p>
@@ -96,43 +132,75 @@ const RegisterForm = ({ onSuccess, loginClicked }) => {
         );
       case 2:
         return (
-          <div className="text-center">
-            <div className="mb-10">
-              <h5 className="text-3xl font-bold mb-3">
-                Generate your password
-              </h5>
-              <p>
-                Both fields should match, to successfully create the password
+          <>
+            <div className="pt-16 px-10 pb-12 max-w-[430px] max-h-[460px] text-center bg-white rounded-2xl">
+              <h2 className="text-2xl font-semibold mb-2">
+                Verify your Phone Number
+              </h2>
+              <p className="text-sm mb-10">
+                To book the visit please enter the OTP sent to +91 {form.phone}.
+              </p>
+
+              <OTPInput onOTPEntered={otpEntered} />
+
+              <Button
+                disabled={form.otp?.length < 4}
+                color="secondary"
+                label="Verify and Proceed"
+                className="mt-10"
+                onClick={registerUser}
+              />
+
+              <p className="mt-14">
+                Didn&apos;t receive your code?{" "}
+                <span className="font-semibold">Resend Code</span>
               </p>
             </div>
-
-            <div className="">
-              <TextInput
-                type={"password"}
-                placeholder={"Password"}
-                name="password"
-                value={form.password}
-                onChange={formValueChanged}
-              />
-              <TextInput
-                type={"password"}
-                placeholder={"Confirm Password"}
-                name="confirm_password"
-                value={form.confirm_password}
-                onChange={formValueChanged}
-              />
-
-              <Button label="Submit" onClick={registerUser} />
-            </div>
-          </div>
+          </>
         );
+      // return (
+      //   <div className="text-center">
+      //     <div className="mb-10">
+      //       <h5 className="text-3xl font-bold mb-3">
+      //         Generate your password
+      //       </h5>
+      //       <p>
+      //         Both fields should match, to successfully create the password
+      //       </p>
+      //     </div>
+
+      //     <div className="">
+      //       <TextInput
+      //         type={"password"}
+      //         placeholder={"Password"}
+      //         name="password"
+      //         value={form.password}
+      //         onChange={formValueChanged}
+      //       />
+      //       <TextInput
+      //         type={"password"}
+      //         placeholder={"Confirm Password"}
+      //         name="confirm_password"
+      //         value={form.confirm_password}
+      //         onChange={formValueChanged}
+      //       />
+
+      //       <Button label="Submit" onClick={registerUser} />
+      //     </div>
+      //   </div>
+      // );
       default:
         return null;
     }
   };
 
   return (
-    <div className="p-10 text-primary bg-white w-full md:w-[430px]">
+    <div
+      className={
+        (currentPage === 2 ? "" : "p-8") +
+        " text-primary bg-white w-full md:w-[430px] rounded-3xl"
+      }
+    >
       {renderPage(currentPage)}
     </div>
   );
