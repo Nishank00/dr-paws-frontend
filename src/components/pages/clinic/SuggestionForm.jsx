c99bde1b0c95f0275b11872538a1a4a64621ce94
Mohammad Salman Khan <salman.khan@gokozo.com>
2024-04-26 02:55:25 +0530

    Minor change

--- src/components/pages/clinic/SuggestionForm.jsx
+++ src/components/pages/clinic/SuggestionForm.jsx
@@ -6,6 +6,16 @@ import PhoneNumberInput from "@/components/ui/PhoneNumberInput";
 const SuggestionForm = () => {
   return (
     <div className="w-full bg-primary3 ">
+      <div className="justify-center w-full items-center shadow-sm bg-primary3 flex  flex-col">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/0c86bbb0d478f3d8bde31d329a0619c24df3bcebaa6c25902d2bc986a973a6de?apiKey=22a36eade5734692978208fb0d2f5c62&"
+          className="aspect-[0.79] object-contain object-center w-[15px] md:w-[45px] overflow-hidden max-w-full mt-1 md:mt-9"
+        />
+        <div className="text-primary text-center  font-custom-roca text-sm md:text-2xl  capitalize self-stretch mt-1 mb-1 md:mt-10 md:mb-9">
+          Where should the next Dr. Paws Clinic be?
+        </div>
+      </div>
       <div className="w-[80%] pt-[50px] m-auto">
         <header className="text-center md:text-left text-xl md:text-2xl text-primary font-custom-roca mb-4">
           Tell us where you’d like a new clinic so we know where to prioritize
--- src/components/pages/team/TeamPage.jsx
+++ src/components/pages/team/TeamPage.jsx
@@ -6,6 +6,7 @@ import Popup from "@/components/ui/Popup";
 import UserService from "@/services/User.Service";
 import ProfilePopupUI from "./ProfilePopupUI";
 import ImageHeader from "@/components/ui/ImageHeader";
+import DoctorProfile from "./DoctorProfile";
 
 const TeamPage = () => {
   const [showPopup, setShowPopup] = useState(false);
@@ -63,7 +64,8 @@ const TeamPage = () => {
       </div>
 
       <Popup isOpen={showPopup} onClose={closePopup}>
-        <ProfilePopupUI selectedDoctor={selectedDoctor} />
+        <DoctorProfile selectedDoctor={selectedDoctor} />
+        {/* <ProfilePopupUI selectedDoctor={selectedDoctor} /> */}
       </Popup>
     </div>
   );
