32d91a6a9d902f53ba8642ed34fe6c018e07f31e
Nishank Sidhpura <nishank@Nishanks-Air.lan>
2024-05-15 17:25:41 +0530

    Changes

--- src/components/pages/booking/BookingConfirmedPage.jsx
+++ src/components/pages/booking/BookingConfirmedPage.jsx
@@ -191,30 +191,38 @@ const BookingConfirmedPage = ({ appointment_id = 0 }) => {
         }  px-24 py-12 flex flex-col items-center rounded-2xl shadow-lg`}
       >
         {appointment?.doctor_name?.toLowerCase() === "groomer" ? (
-          <h3 className="text-2xl font-extrabold ">
-            {pets?.map((pet) => pet?.pet_name).join(", ")}&nbsp;
-            {pets?.length > 1 ? "are" : "is"} booked for a Grooming session
+  <h3 className="text-2xl font-extrabold">
+    {pets?.length === 1
+      ? `${pets[0]?.pet_name} is booked for a Grooming session`
+      : pets?.length === 2
+      ? `${pets[0]?.pet_name} & ${pets[1]?.pet_name} are booked for a Grooming session`
+      : `${pets?.slice(0, -1).map((pet) => pet?.pet_name).join(", ")} & ${
+          pets?.[pets.length - 1]?.pet_name
+        } are booked for a Grooming session`}
+  </h3>
+) : (
+  <>
+    {pets?.every((pet) => pet.service_name === pets[0].service_name) ? (
+      <h3 className="text-2xl font-extrabold">
+        {pets?.length === 1
+          ? `${pets[0]?.pet_name} is booked for a Vet appointment`
+          : pets?.length === 2
+          ? `${pets[0]?.pet_name} & ${pets[1]?.pet_name} are booked for a Vet appointment`
+          : `${pets?.slice(0, -1).map((pet) => pet?.pet_name).join(", ")} & ${
+              pets?.[pets.length - 1]?.pet_name
+            } are booked for a Vet appointment`}
+      </h3>
+    ) : (
+      <>
+        {pets?.map((pet, index) => (
+          <h3 key={index} className="text-2xl font-extrabold">
+            {`${pet?.pet_name} is booked for a Vet appointment`}
           </h3>
-        ) : (
-          <h3 className="text-2xl font-extrabold">
-            {pets?.length === 1
-              ? pets[0]?.pet_name
-              : pets?.length === 2
-              ? pets
-                  .map((pet, index) =>
-                    index === 0 ? pet?.pet_name : ` & ${pet?.pet_name}`
-                  )
-                  .join("")
-              : pets
-                  ?.slice(0, -1)
-                  .map((pet) => pet?.pet_name)
-                  .join(", ") +
-                " & " +
-                pets?.[pets.length - 1]?.pet_name}
-            &nbsp;{pets?.length > 1 ? "are" : "is"} booked for a Grooming
-            session
-          </h3>
-        )}
+        ))}
+      </>
+    )}
+  </>
+)}
         {/* {appointment?.doctor_name?.toLowerCase() !== "groomer" && (
           <p className="text-lg">
             for&nbsp;
