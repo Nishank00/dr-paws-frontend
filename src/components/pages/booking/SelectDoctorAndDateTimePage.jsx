76193041d8f4349f539e9d7d800df9005c15be94
Nishank Sidhpura <nishanksidhpura@gmail.com>
2024-05-23 12:19:10 +0530

    Changes

--- src/components/pages/booking/SelectDoctorAndDateTimePage.jsx
+++ src/components/pages/booking/SelectDoctorAndDateTimePage.jsx
@@ -5,6 +5,7 @@ import moment from "moment";
 import { useRouter } from "next/navigation";
 import DoctorService from "@/services/Doctor.Service";
 import { useToast } from "@/components/ui/ToastProvider";
+// import Loader from "@/components/ui/Loader";
 
 const SelectDoctorAndDateTimePage = ({
   doctors = [],
@@ -19,66 +20,59 @@ const SelectDoctorAndDateTimePage = ({
   isGroomingOnly = false,
   selectedServicesData = [],
 }) => {
-
-  
-
   const [isDoctorSelected, setIsDoctorSelected] = useState(false);
-  const [availableSlots, setAvailableSlots] = useState([]);
+  const [availableTimeSlots, setAvailableTimeSlots] = useState([]);
   const [selectedDoctorId, setSelectedDoctorId] = useState(null);
   const [availableDays, setAvailableDays] = useState([]);
   const [doctorClinicTimings, setDoctorClinicTimings] = useState([]);
+  const [isLoadingDoctorClinicData, setIsLoadingDoctorClinicData] = useState(false);
   const showToast = useToast();
 
-  const dateSelected = async (date) => {
+  // Handle date selection
+  const handleDateSelection = async (date) => {
     setSelectedDate(date);
-    await getSelectedDoctorClinicData(date);
+    setIsLoadingDoctorClinicData(true);
+    await fetchDoctorClinicData(date);
+    setIsLoadingDoctorClinicData(false);
   };
 
-  console.log("availableDays", availableDays);
-
-  const slotClicked = (availableSlot) => {
-    setAvailableSlots(
-      availableSlots.map((slot) => {
-        if (slot.formattedTime == availableSlot.formattedTime) {
-          slot.selected = true;
-          setSelectedSlot({ ...availableSlot, selected: true });
-        } else {
-          slot.selected = false;
-        }
-        return slot;
-      })
+  // Handle time slot selection
+  const handleTimeSlotSelection = (selectedTimeSlot) => {
+    setAvailableTimeSlots(
+      availableTimeSlots.map((slot) => ({
+        ...slot,
+        selected: slot.formattedTime === selectedTimeSlot.formattedTime,
+      }))
     );
+    setSelectedSlot(selectedTimeSlot);
   };
 
-  async function handleDoctorClick(doctorIndex, doctorId, doctorName) {
+  // Handle doctor selection
+  const handleDoctorSelection = async (doctorId) => {
     console.log("Doctor Clicked", doctorId);
-    let copyDoctorData = doctors.map((doctor) => {
-      if (doctor.id === doctorId) {
-        doctor["selected"] = !doctor["selected"];
-      } else {
-        doctor["selected"] = false;
-      }
-      return { ...doctor };
-    });
+    const updatedDoctors = doctors.map((doctor) => ({
+      ...doctor,
+      selected: doctor.id === doctorId,
+    }));
 
-    console.log(doctors)
+    console.log(updatedDoctors);
 
     setSelectedDoctorId(doctorId);
-    setDoctors(copyDoctorData);
+    setDoctors(updatedDoctors);
 
-   
-      // Fetch the selected doctor's clinic data
-      await getSelectedDoctorClinicData();
-   
-  }
+    setIsLoadingDoctorClinicData(true);
+    await fetchDoctorClinicData();
+    setIsLoadingDoctorClinicData(false);
+  };
 
-  async function getSelectedDoctorClinicData(date = null) {
+  // Fetch doctor clinic data based on selected doctor and date
+  const fetchDoctorClinicData = async (date = null) => {
     if (selectedDoctorId === "best-available-vet") {
       try {
         const availableDoctors = doctors.filter(
           (doctor) => doctor.id !== "best-available-vet"
         );
-  
+
         const allDoctorTimings = await Promise.all(
           availableDoctors.map(async (doctor) => {
             const payload = {
@@ -90,105 +84,109 @@ const SelectDoctorAndDateTimePage = ({
             return response.data.data;
           })
         );
-  
+
         console.log("All Doctor Timings", allDoctorTimings);
-        
-        if(date) {
-          const availableSlots = allDoctorTimings.map((doctorTimings) => doctorTimings.timesArray).flat();
-          setAvailableSlots(availableSlots);
+
+        if (date) {
+          const availableSlots = allDoctorTimings
+            .map((doctorTimings) => doctorTimings.timesArray)
+            .flat();
+          setAvailableTimeSlots(availableSlots);
         } else {
-          const availableDays = allDoctorTimings.map((doctorTimings) => doctorTimings.availableDays).flat();
+          const availableDays = allDoctorTimings
+            .map((doctorTimings) => doctorTimings.availableDays)
+            .flat();
           setAvailableDays(availableDays);
         }
-       
-
-       
       } catch (error) {
         console.log(error);
+        showToast("Failed to fetch doctor clinic data", "error");
       }
     } else {
       try {
-        let payload = {
+        const payload = {
           doctor_id: selectedDoctorId,
           clinic_id: selectedClinic.id,
+          date: date ? moment(date).format("YYYY-MM-DD") : null,
         };
-  
-        if (date) {
-          payload["date"] = moment(date).format("YYYY-MM-DD");
-        }
-  
+
         const response = await DoctorService.getDoctorClinicTimings(payload);
-        const { data = { status: false, message: "Something is missing" } } = response;
-        if (!data.status) return showToast(data.message);
-  
+        const { data = { status: false, message: "Something is missing" } } =
+          response;
+        if (!data.status) return showToast(data.message, "error");
+
         setDoctorClinicTimings(data.data);
         setAvailableDays(data.data?.availableDays);
-        setAvailableSlots(data.data?.timesArray);
+        setAvailableTimeSlots(data.data?.timesArray);
       } catch (error) {
         console.log(error);
+        showToast("Failed to fetch doctor clinic data", "error");
       }
     }
-  }
+  };
 
+  // Sort doctors with "Best Available Vet" at the top
   const sortedDoctors = [...doctors].sort((a, b) => {
     if (a.doctor_name.toLowerCase() === "best available vet") return -1;
     if (b.doctor_name.toLowerCase() === "best available vet") return 1;
     return 0;
   });
 
+  // Check if a day is available for booking
   const isDayAvailable = (selectedDate) => {
-  // if (isGroomingOnly) {
-    //   return true;
-    // }
     const selectedDay = moment(selectedDate).format("dddd");
     console.log("selectedDay", selectedDay);
     return availableDays.includes(selectedDay);
   };
 
+  // Reset available time slots when selected doctor changes
   useEffect(() => {
-    setAvailableSlots([]);
-    getSelectedDoctorClinicData();
+    setAvailableTimeSlots([]);
+    setIsLoadingDoctorClinicData(true);
+    fetchDoctorClinicData();
+    setIsLoadingDoctorClinicData(false);
   }, [selectedDoctorId]);
 
+  // Update available days when doctor clinic timings change
   useEffect(() => {
-    // console.log({ doctorClinicTimings });
-    let { availableDays = [] } = doctorClinicTimings;
     console.log("doctorClinicTimings", doctorClinicTimings);
-    setAvailableDays(availableDays);
+    setAvailableDays(doctorClinicTimings.availableDays || []);
   }, [doctorClinicTimings]);
 
+  // Fetch doctor clinic data when selected date changes
   useEffect(() => {
-    if (selectedDate) dateSelected(selectedDate);
+    if (selectedDate) handleDateSelection(selectedDate);
   }, [selectedClinic]);
 
+  // Update isDoctorSelected state when doctors change
   useEffect(() => {
-    setIsDoctorSelected(doctors.filter((doctor) => doctor.selected).length > 0);
+    setIsDoctorSelected(doctors.some((doctor) => doctor.selected));
   }, [doctors]);
 
-  // UseEffect to set first doctor if isGroomingOnly
+  // Set first doctor as selected if isGroomingOnly is true
   useEffect(() => {
     console.log("isGroomingOnly", isGroomingOnly);
-    if (isGroomingOnly ) {
+    if (isGroomingOnly && doctors.length > 0) {
       console.log("Making first doctor selected");
       const firstDoctorId = doctors[0].id;
       setSelectedDoctorId(firstDoctorId);
-  
+
       const updatedDoctors = doctors.map((doctor, index) => ({
         ...doctor,
         selected: index === 0,
       }));
       setDoctors(updatedDoctors);
-  
-      // Fetch the first doctor's clinic data
-      getSelectedDoctorClinicData();
-    }
-  }, [isGroomingOnly, selectedDoctorId]);
 
+      setIsLoadingDoctorClinicData(true);
+      fetchDoctorClinicData();
+      setIsLoadingDoctorClinicData(false);
+    }
+  }, [isGroomingOnly, doctors]);
 
   return (
     <div className={"pt-1 sm:pt-10 " + className}>
       <div className="mb-24">
-        {!doctors || doctors.length == 0 || !selectedClinic ? (
+        {!doctors || doctors.length === 0 || !selectedClinic ? (
           <>
             <h2 className="text-primary text-xl sm:text-4xl font-custom-roca font-medium mb-1">
               Clinic not selected!!
@@ -199,8 +197,8 @@ const SelectDoctorAndDateTimePage = ({
           </>
         ) : (
           <>
-           {true && (
-              <div style={{ display: isGroomingOnly ? 'none' : 'block' }}>
+            {!isGroomingOnly && (
+              <div>
                 <h2 className="text-primary text-xl sm:text-4xl font-custom-roca font-medium mb-1">
                   Select Vet
                 </h2>
@@ -214,80 +212,84 @@ const SelectDoctorAndDateTimePage = ({
                       key={"doctor" + i}
                       doctor={{ ...doctor, index: i }}
                       selected={doctor?.selected}
-                      onClick={handleDoctorClick}
+                      onClick={() => handleDoctorSelection(doctor.id)}
                     />
                   ))}
                 </div>
               </div>
             )}
             <div className="pt-1 sm:pt-10">
-              <h2 onClick={
-                () => {
-              
-                }
-              } className="text-primary text-xl sm:text-4xl font-custom-roca font-medium mb-1">
+              <h2 className="text-primary text-xl sm:text-4xl font-custom-roca font-medium mb-1">
                 Select Date and Time
               </h2>
               <p className="text-primary text-xs sm:text-sm mb-4 font-custom-open-sans">
-                Please select your appoinment slot
+                Please select your appointment slot
               </p>
-              { !selectedDoctorId && currentPage == 3 && (
+              {!selectedDoctorId && currentPage === 3 && (
                 <p className="text-red-500 mb-6">Select Doctor</p>
               )}
               <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-3 pb-10 items-start justify-start">
                 <div className="col-span-2">
                   <Calendar
                     selected={selectedDate}
-                    onSelect={dateSelected}
+                    onSelect={handleDateSelection}
                     disabled={!isDoctorSelected}
                     availableDays={availableDays}
                   />
                 </div>
                 <div className="text-primary font-semibold">
-                  {selectedDate && (
+                  {isLoadingDoctorClinicData ? (
+                    <Loader />
+                  ) : (
                     <>
-                      {isDayAvailable(selectedDate) ? (
-                        <h5 className="text-black mb-5">
-                          Available timeslots for{" "}
-                          <span className="text-primary">
-                            {moment(selectedDate).format("dddd, MMM D")}
-                          </span>
-                        </h5>
-                      ) : (
-                        <p className="text-danger">
-                          Sorry, no available timeslots for this day. We do
-                          welcome walk-ins but {`can't `}guarantee an appointment
-                          time.
-                        </p>
-                      )}
+                      {selectedDate && (
+                        <>
+                          {isDayAvailable(selectedDate) ? (
+                            <h5 className="text-black mb-5">
+                              Available timeslots for{" "}
+                              <span className="text-primary">
+                                {moment(selectedDate).format("dddd, MMM D")}
+                              </span>
+                            </h5>
+                          ) : (
+                            <p className="text-danger">
+                              Sorry, no available timeslots for this day. We do
+                              welcome walk-ins but {`can't`} guarantee an
+                              appointment time.
+                            </p>
+                          )}
 
-                      <div className="">
-                        {availableSlots &&
-                          availableSlots.map((avaliableSlot, index) => (
-                            <div key={"slot" + index}>
-                              {avaliableSlot.selected ? (
-                                <div className="grid grid-cols-2 gap-2 w-full sm:max-w-60">
-                                  <div className="rounded-lg shadow-lg bg-primary4 border-2 border-primary w-full sm:w-30 py-4 flex items-center justify-center mb-3">
-                                    <span>{avaliableSlot.formattedTime}</span>
-                                  </div>
-                                  <button
-                                    onClick={onConfirmBooking}
-                                    className="rounded-lg shadow-lg bg-secondary hover:bg-primary text-white border-2 border-secondary w-full sm:w-30 py-4 flex items-center justify-center mb-3"
-                                  >
-                                    Confirm
-                                  </button>
+                          <div className="">
+                            {availableTimeSlots &&
+                              availableTimeSlots.map((timeSlot, index) => (
+                                <div key={"slot" + index}>
+                                  {timeSlot.selected ? (
+                                    <div className="grid grid-cols-2 gap-2 w-full sm:max-w-60">
+                                      <div className="rounded-lg shadow-lg bg-primary4 border-2 border-primary w-full sm:w-30 py-4 flex items-center justify-center mb-3">
+                                        <span>{timeSlot.formattedTime}</span>
+                                      </div>
+                                      <button
+                                        onClick={onConfirmBooking}
+                                        className="rounded-lg shadow-lg bg-secondary hover:bg-primary text-white border-2 border-secondary w-full sm:w-30 py-4 flex items-center justify-center mb-3"
+                                      >
+                                        Confirm
+                                      </button>
+                                    </div>
+                                  ) : (
+                                    <div
+                                      onClick={() =>
+                                        handleTimeSlotSelection(timeSlot)
+                                      }
+                                      className="rounded-lg shadow-lg bg-primary4 w-full sm:max-w-60 py-4 flex items-center justify-center mb-3 cursor-pointer hover:shadow-2xl"
+                                    >
+                                      <span>{timeSlot.formattedTime}</span>
+                                    </div>
+                                  )}
                                 </div>
-                              ) : (
-                                <div
-                                  onClick={() => slotClicked(avaliableSlot)}
-                                  className="rounded-lg shadow-lg bg-primary4 w-full sm:max-w-60 py-4 flex items-center justify-center mb-3 cursor-pointer hover:shadow-2xl"
-                                >
-                                  <span>{avaliableSlot.formattedTime}</span>
-                                </div>
-                              )}
-                            </div>
-                          ))}
-                      </div>
+                              ))}
+                          </div>
+                        </>
+                      )}
                     </>
                   )}
                 </div>
