6d3d5105ed4b6d84c2f6d0f7dee3dc9634e1d539
Nishank Sidhpura <nishanksidhpura@gmail.com>
2024-05-24 06:52:20 +0530

    Add Custom Avatars in UserReviews

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     images: {
-        domains: ['s3-alpha-sig.figma.com', 'example.com', 'another-example.com', 'localhost'],
+        domains: ['s3-alpha-sig.figma.com', 'example.com', 'another-example.com', 'localhost', 'avatar.iran.liara.run'],
     },
 }
