243a732231028effc445074af7e8c4d361b5903b
Prerak siddhpura <prerak.siddhpura@gmail.com>
2024-05-28 14:01:14 +0530

    updated the book a visit card in mobile version

--- src/components/pages/clinic/ClinicCard.jsx
+++ src/components/pages/clinic/ClinicCard.jsx
@@ -14,10 +14,10 @@ const ClinicCard = ({ imageUrl = "/home/clinic_image.png", clinic = {} }) => {
     );
   }, [services]);
   return (
-    <div className="bg-primary4 relative rounded-md cursor-pointer w-[80%] m-auto lg:w-[100%] shadow-md flex flex-col transition ease-in-out delay-500 hover:shadow-lg">
+    <div className="bg-primary4 relative rounded-md cursor-pointer w-[90%] m-auto lg:w-[100%] shadow-md flex flex-col transition ease-in-out delay-500 hover:shadow-lg">
       <div
         onClick={() => router.push(`/clinics/overview/${id}`)}
-        className="rounded-t-md w-full h-[92px] md:h-[240px] flex justify-end"
+        className="rounded-t-md w-full h-[150px] lg:h-[140px] md:h-[240px] flex justify-end"
         style={{
           backgroundImage: "url(" + imageUrl + ")",
           backgroundPosition: "center",
@@ -26,7 +26,7 @@ const ClinicCard = ({ imageUrl = "/home/clinic_image.png", clinic = {} }) => {
         }}
       >
         {clinic?.is_24_hr == "Y" ? (
-          <div className="text-primary px-2 h-7 absolute top-1 right-1 rounded-t-md text-sm md:text-base text-center font-bold font-custom-open-sans bg-primary4 rounded-lg flex items-center justify-center">
+          <div className="text-primary  px-2 h-7 absolute top-1 right-1 rounded-t-md text-lg md:text-base text-center font-bold font-custom-open-sans bg-primary4 rounded-lg flex items-center justify-center">
             <Clock /> &nbsp; 24 Hours
           </div>
         ) : (
@@ -35,16 +35,16 @@ const ClinicCard = ({ imageUrl = "/home/clinic_image.png", clinic = {} }) => {
       </div>
       <div
         onClick={() => router.push(`/clinics/overview/${id}`)}
-        className="pl-6 pr-2 py-4 text-primary grow"
+        className="pl-6 pr-2 py-4  text-primary grow"
       >
-        <h4 className="font-semibold font-custom-open-sans text-xs md:text-[18px] line-clamp-1">
+        <h4 className="font-semibold font-custom-open-sans lg:text-lg text-xl md:text-[18px] line-clamp-1">
           Dr. Paw | {name}
         </h4>
-        <p className=" text-[10px] md:text-[12px] font-custom-open-sans mt-2 text-primary text-wrap">
+        <p className=" lg:text-[10px] text-[15px] md:text-[12px] font-custom-open-sans mt-2 text-primary text-wrap">
           Check-Ups | Surgery | Diagnostics | Grooming | Retail
         </p>
       </div>
-      <BookingButton className="text-center text-white text-xs md:text-sm transition h-[30px] md:h-[50px] font-bold font-custom-open-sans bg-secondary hover:bg-primary flex justify-center items-center rounded-b-md" />
+      <BookingButton className="text-center text-white text-lg py-6 md:text-sm transition h-[30px] md:h-[50px] font-bold font-custom-open-sans bg-secondary hover:bg-primary flex justify-center items-center rounded-b-md" />
     </div>
   );
 };
